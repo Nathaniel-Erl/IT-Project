553b71878fd6e72051846fc240439619159cf704
Zhi Yang <1zhiyang1@gmail.com>
2022-09-04 06:20:46 +1000

    added a nav bar

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,17 @@
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, world!</h1>);
+root.render(
+  <root>
+    <div class="topnav">
+      <a class="active" href="#home">
+        Home
+      </a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+    <h1>Hello, world!</h1>
+  </root>
+);
