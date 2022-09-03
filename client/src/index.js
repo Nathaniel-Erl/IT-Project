6ac931d18334b4830ae7d03651d2a1771267c59b
Zhi Yang <1zhiyang1@gmail.com>
2022-09-04 07:27:19 +1000

    added stuff for dashboard, not formatted

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,40 @@
 import ReactDOM from "react-dom";
 import "./styles.css";
+import profile from "./images/profile.png";
+import badge1 from "./images/badge1.png";
+import badge2 from "./images/badge2.png";
+import badge3 from "./images/badge3.png";
+import badge4 from "./images/badge4.png";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <root>
-    <div class="topnav">
+    {/* nav bar */}
+    <nav class="topnav">
       <a class="active" href="#home">
         Home
       </a>
       <a href="#news">News</a>
       <a href="#contact">Contact</a>
       <a href="#about">About</a>
-    </div>
+    </nav>
+
+    {/* user */}
+    <user>
+      <profile>
+        <img src={profile} alt="profile" />
+        <name>Test123</name>
+      </profile>
+      <badgeGroup>
+        <p>Badges</p>
+        <badges>
+          <img src={badge1} alt="badge1" />
+          <img src={badge2} alt="badge2" />
+          <img src={badge3} alt="badge3" />
+          <img src={badge4} alt="badge4" />
+        </badges>
+      </badgeGroup>
+    </user>
     <h1>Hello, world!</h1>
   </root>
 );
