84c720ab1d189a3c68ad634c0ad0b8cc57af2b55
Zhi Yang <1zhiyang1@gmail.com>
2022-09-16 21:54:51 +1000

    dashboard finished

--- client/src/components/ProfileBlock/ProfileBlock.js
+++ client/src/components/ProfileBlock/ProfileBlock.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { Avatar } from "@mui/material";
+import { Avatar, colors, LinearProgress, Typography } from "@mui/material";
 import * as constants from "../../static/Constants.js";
 import badge1 from "../../images/badge1.png";
 import badge2 from "../../images/badge2.png";
@@ -9,22 +9,34 @@ import badge4 from "../../images/badge4.png";
 import profile from "../../images/profile.png";
 import { Grid } from "@mui/material";
 import { StyledAppBar } from "../NavBar/styles";
+import { BLACK } from "../../static/Colors.js";
 
 /**
  * @returns returns stuff on profile
  */
 const ProfileBlock = () => {
+  const badges = [badge1, badge2, badge3, badge4];
   return (
     <>
-      <StyledAppBar sx={{ top: 64 }}>
+      <StyledAppBar sx={{ top: 64, minHeight: 1 }}>
         <Grid
           container
           direction="column"
           justifyContent="center"
           alignItems="center"
-          // sx={{ borderLeft: 2, borderColor: colors.GREY }}
         >
-          <Grid item xs={12} sx={{ justifyContent: "center" }}>
+          <Grid
+            item
+            pt={2}
+            sx={{
+              minWidth: 1,
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: BLACK,
+            }}
+          >
             <Avatar
               sx={{
                 height: constants.ICON_MEDIUM,
@@ -35,43 +47,63 @@ const ProfileBlock = () => {
             />
             Username
           </Grid>
-          <Grid item xs={12}>
+          <Grid
+            item
+            pt={2}
+            sx={{
+              minWidth: 1,
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: BLACK,
+            }}
+          >
             Badges
-            <Avatar
+            {/* badges container */}
+            <Grid
+              container
               sx={{
-                height: constants.ICON_SMALL,
-                width: constants.ICON_SMALL,
+                minWidth: 1,
+                justifyContent: "center",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                color: BLACK,
               }}
-              src={badge1}
-              alt="badge1"
-            />
-            <Avatar
-              sx={{
-                height: constants.ICON_SMALL,
-                width: constants.ICON_SMALL,
-              }}
-              src={badge2}
-              alt="badge2"
-            />
-            <Avatar
-              sx={{
-                height: constants.ICON_SMALL,
-                width: constants.ICON_SMALL,
-              }}
-              src={badge3}
-              alt="badge3"
-            />
-            <Avatar
-              sx={{
-                height: constants.ICON_SMALL,
-                width: constants.ICON_SMALL,
-              }}
-              src={badge4}
-              alt="badge4"
-            />
+            >
+              {badges.map((badge) => (
+                <Grid item p={0.3}>
+                  <Avatar
+                    sx={{
+                      height: constants.ICON_SMALL,
+                      width: constants.ICON_SMALL,
+                    }}
+                    src={badge}
+                    alt={badge}
+                  />
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
+          <Grid
+            item
+            pt={2}
+            sx={{
+              minWidth: 1,
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: BLACK,
+            }}
+          >
             Level 999
+            <Grid container p={1}>
+              <Grid xs item>
+                <LinearProgress value={50} variant="determinate" title="test" />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </StyledAppBar>
