5661c8adfc35855008260bfe1c42659cc59ba12f
Zhi Yang <1zhiyang1@gmail.com>
2022-09-17 06:07:20 +1000

    added navbar to signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,38 +1,44 @@
+import React from "react";
+import NavBar from "../components/NavBar/NavBar";
+
 function Signup() {
   return (
-    <div className="App">
-      <form>
-        <label>
-          First Name:
-          <input type="text" name="name" />
-        </label>
-      </form>
-      <form>
-        <label>
-          Last Name:
-          <input type="text" name="name" />
-        </label>
-      </form>
-      <form>
-        <label>
-          Username:
-          <input type="text" name="name" />
-        </label>
-      </form>
-      <form>
-        <label>
-          Email:
-          <input type="text" name="name" />
-        </label>
-      </form>
-      <form>
-        <label>
-          Password:
-          <input type="text" name="name" />
-        </label>
-      </form>
-      <input type="submit" value="Submit" />
-    </div>
+    <>
+      <NavBar></NavBar>
+      <div className="App">
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Last Name:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Username:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Email:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Password:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <input type="submit" value="Submit" />
+      </div>
+    </>
   );
 }
 
