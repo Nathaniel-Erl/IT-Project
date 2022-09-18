1eca7c55a2567e42352af9652f3be278490a63d9
Nathaniel-Erl <nathanielerl@gmail.com>
2022-09-19 05:37:42 +1000

    implemented user authentication with passport and JSON web tokens

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose'
-import Question from './questionSchema.js'
-import Quiz from './quizSchema.js'
+import mongoose from "mongoose";
+import Question from "./questionSchema.js";
+import Quiz from "./quizSchema.js";
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -8,9 +8,8 @@ const userSchema = mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  quizzes: { type: Quiz.schema, required: false },
-})
+});
 
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model("user", userSchema);
 
-export default User
+export default User;
---
+++ server/validation/sign-up.js
@@ -0,0 +1,1 @@
+// TODO: implement user sign up form validation
