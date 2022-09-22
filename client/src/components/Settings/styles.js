6e2010c467115efbccb13ad1dc256d243c53b938
SRFleming <sreyfleming@student.unimelb.edu.au>
2022-09-22 20:38:51 +1000

    Settings Page Mobile Compatibility

---
+++ client/src/components/Settings/styles.js
@@ -0,0 +1,49 @@
+import { Box, styled } from "@mui/material";
+
+const SABox = styled(Box)(({ theme }) => ({
+  width: "30vw",
+  borderRadius: "4px",
+  backgroundColor: "#FFFFFF",
+  padding: "20px",
+  margin: "0vw",
+  overflow: "hidden",
+  overflowY: "hidden",
+  [theme.breakpoints.down("sm")]: {
+    width: "auto",
+    borderRadius: "0",
+    padding: "5px",
+    width: "80vw",
+  },
+  [theme.breakpoints.down("md")]: {
+    borderRadius: "0",
+    padding: "20px",
+    width: "80vw",
+  },
+}));
+
+const Form = styled("form")({
+  autoComplete: "off",
+  noValidate: "true",
+  display: "flex",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  gap: "1rem",
+});
+
+const MainContainer = styled(Box)(({ theme }) => ({
+  height: "89vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  textalign: "center",
+  [theme.breakpoints.down("sm")]: {
+    display: "grid",
+    height: "70vh",
+  },
+  [theme.breakpoints.down("md")]: {
+    display: "grid",
+    height: "auto",
+  },
+}));
+
+export { SABox, Form, MainContainer };
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -2,26 +2,29 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
-import SignUpForm from "../components/SignUp/SignUpForm";
-import SignOutButton from "../components/Buttons/SignOutButton";
+import SignUpForm from "../components/Settings/SettingsForm.js";
+import SignOutButton from "../components/Buttons/SignOutButton.js";
+import { MainContainer } from "../components/Settings/styles.js";
 
 function Settings() {
   return (
     <>
       <NavBar title="Settings" />
       {/* contains everything */}
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid container display="flex" justifyContent="center" padding={3}>
-          <Grid item xs={6}>
-            <SignUpForm title="Settings" button="Save Changes" link="../dashboard" />
+      <MainContainer>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid>
+            <Grid item xs={6}>
+              <SignUpForm title="Settings" button="Save Changes" link="../dashboard" />
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </MainContainer>
       <SignOutButton title="Log Out" link="/login"></SignOutButton>
     </>
   );
