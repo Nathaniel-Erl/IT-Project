b7873fcaa1c7a9305d1af6e57f3bc5bf01e2cd56
Nathaniel-Erl <nathanielerl@gmail.com>
2022-09-27 13:40:26 +1000

    updated api index for testing deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,20 @@
-import axios from 'axios'
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+import axios from "axios";
+//const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: "https://quizy-it-project.herokuapp.com/",
+});
 
 // quiz
-export const fetchQuizzes = () => API.get('/quiz')
-export const createQuiz = (newQuiz) => API.post('/quiz', newQuiz)
-export const deleteQuiz = (id) => API.delete(`/quiz/${id}`)
-export const updateQuiz = (id, updatedQuiz) => API.patch(`/quiz/${id}`, updatedQuiz);
+export const fetchQuizzes = () => API.get("/quiz");
+export const createQuiz = (newQuiz) => API.post("/quiz", newQuiz);
+export const deleteQuiz = (id) => API.delete(`/quiz/${id}`);
+export const updateQuiz = (id, updatedQuiz) =>
+  API.patch(`/quiz/${id}`, updatedQuiz);
 
 // question
 export const getAllQuestions = (quizId) => API.get(`/quiz/${quizId}`);
-export const createQuestion = (newQuestion) => API.post('/createquestion', newQuestion)
+export const createQuestion = (newQuestion) =>
+  API.post("/createquestion", newQuestion);
 
 // auth
 export const logIn = (formData) => API.post("/", formData);
