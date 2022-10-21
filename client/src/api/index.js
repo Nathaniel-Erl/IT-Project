e86fd80b8b97e9fa9f8398f24b25878e5d18e1d2
rgokool <r18.gokool@gmail.com>
2022-10-21 12:16:57 +1100

    added back updateQuestion to index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,39 +1,22 @@
 import axios from 'axios'
-import { authHeader } from '../actions/auth.js'
 const url = 'https://quizy-it-project.herokuapp.com/'
 const local = 'http://localhost:5000'
 const API = axios.create({ baseURL: url })
 // quiz
-export const fetchQuizzes = () =>
-  API.get('/quiz', {
-    headers: authHeader(),
-  })
-export const createQuiz = (newQuiz) =>
-  API.post('/quiz', newQuiz, {
-    headers: authHeader(),
-  })
-export const deleteQuiz = (id) =>
-  API.delete(`/quiz/${id}`, {
-    headers: authHeader(),
-  })
+export const fetchQuizzes = () => API.get('/quiz')
+export const createQuiz = (newQuiz) => API.post('/quiz', newQuiz)
+export const deleteQuiz = (id) => API.delete(`/quiz/${id}`)
 export const updateQuiz = (id, updatedQuiz) =>
-  API.patch(`/quiz/${id}`, updatedQuiz, {
-    headers: authHeader(),
-  })
+  API.patch(`/quiz/${id}`, updatedQuiz)
 
 // question
-export const getAllQuestions = (quizId) =>
-  API.get(`/quiz/${quizId}`, {
-    headers: authHeader(),
-  })
+export const getAllQuestions = (quizId) => API.get(`/quiz/${quizId}`)
 export const createQuestion = (newQuestion) =>
-  API.post('/createquestion', newQuestion, {
-    headers: authHeader(),
-  })
+  API.post('/createquestion', newQuestion)
 export const deleteQuestion = (quizId, questionId) =>
-  API.delete(`/quiz/${quizId}/${questionId}`, {
-    headers: authHeader(),
-  })
+  API.delete(`/quiz/${quizId}/${questionId}`)
+export const updateQuestion = (quizId, questionId, updatedQuestion) =>
+  API.patch(`/quiz/${quizId}/${questionId}`, updatedQuestion)
 
 // auth
 export const logIn = (formData) => API.post('/', formData)
